f2d2b2ade7011e328484710167a2e813b65a07e8
oajh04 <cloudtubekr@naver.com>
2021-01-19 15:32:31 +0900

    SearchStyle

--- components/styled/SearchResult/SearchResultStyle.js
+++ components/styled/SearchResult/SearchResultStyle.js
@@ -182,3 +182,9 @@ export const ResultAddNumber = styled.div`
         height: 15px;
     }
 `;
+
+export const noneData = styled.div`
+    font-size: 50px;
+    text-align: center;
+    align-items: center;
+`
